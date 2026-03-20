dbd5044c5939f2a54cf37020a2cdc69fdad594bc
zalmonx <jigsaw11791@gmail.com>
2026-03-20 13:28:17 +0700

    change basename

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
   { path: "/Info", element: <Info /> },
   { path: "/Model", element: <Model /> },
 ], {
-  basename: "/ilovemythesisverymuch/",
+  basename: "/DinoLearn/",
 });
 
 const App = () => {
